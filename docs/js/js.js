b996a870d0957ed7a6afe854c0082ac82060266e
Carlo Alemán <caleman9791@gmail.com>
2024-11-01 20:37:44 -0400

    Variables para texto corregido.

--- docs/js/js.js
+++ docs/js/js.js
@@ -6,19 +6,60 @@ window.onload = (event) => {
 
     let nombreEnNav = document.querySelector(".navbar-brand span");
 
-    nombreEnNav.textContent = "Demostración";
+    if (nombreEnNav) {
+        console.log(nombreEnNav);
+        nombreEnNav.textContent = "Demostración";
+    }
 
     let work_sectionText = document.querySelector(".work_section .custom_heading-container h3");
-    if (work_sectionText) {
+
+    if (work_sectionText != null) {
+        console.log(work_sectionText);
         work_sectionText.textContent = "¿Cómo funiona?";
     }
 
+    let enlaceAboutText = document.querySelectorAll(`[href="about.html"]`);
 
-    let enlaceAboutText = document.querySelectorAll(`[href="about.html]`);
+    if (enlaceAboutText) {
+        console.log(enlaceAboutText);
+        for (var i = 0; i < enlaceAboutText.length; i++) {
 
-    for (var i = 0; i < enlaceAboutText.length; i++) {
-        enlaceAboutText[i].textContent = `Sobre nosotros`;
+            enlaceAboutText[i].textContent = `Sobre nosotros`;
+        }
+
     }
+
+    let enlaceServiceText = document.querySelectorAll(`[href="service.html"]`);
+
+    if (enlaceServiceText) {
+        console.log(enlaceServiceText);
+        for (var i = 0; i < enlaceServiceText.length; i++) {
+
+            enlaceServiceText[i].textContent = `Servicios`;
+        }
+
+    }
+    let enlacecontactText = document.querySelectorAll(`[href="contact.html"]`);
+
+    if (enlacecontactText) {
+
+        for (var i = 0; i < enlacecontactText.length; i++) {
+
+            enlacecontactText[i].textContent = `Contactamos`;
+        }
+
+    }
+    let enlaceinicioText = document.querySelectorAll(`[href="#"]`);
+
+    if (enlaceinicioText) {
+
+        for (var i = 0; i < enlaceinicioText.length; i++) {
+
+            enlaceinicioText[i].textContent = `inicio`;
+        }
+
+    }
+
     // work_sectionText.textContent = "¿Cómo funiona?";
     // about.html
 };
